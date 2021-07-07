314bc2a003dff0315c4a80d8d021c70e8059a9d5
Armand Philippot <git@armandphilippot.com>
2021-07-07 18:22:48 +0200

    refactor(zsh): remove app name from title to speed up Zsh
    Finally it's not a very useful info and removing it allows me to speed
    up the terminal load time.

--- home/xdg_config/zsh/includes/title.zsh
+++ home/xdg_config/zsh/includes/title.zsh
@@ -1,32 +1,24 @@
 autoload -Uz add-zsh-hook
 
-get_app_name() {
-	ps --pid $(ps --pid $$ -o ppid=) -o comm=
-}
-
 title_precmd() {
-	local _app_name=$(get_app_name) || true
-
 	case $TERM in
 	'screen'*|'tmux'*)
 		print -Pn -- "\e_\005{g}%n\005{-}@\005{m}%m\005{-} \005{B}%~\005{-}\e\\"
 		;;
 	*)
-		print -Pn -- "\e]2;${_app_name} | %n@%m > %~\a"
+		print -Pn -- "\e]2;%n@%m > %~\a"
 		;;
 	esac
 }
 
 title_preexec() {
-	local _app_name=$(get_app_name) || true
-
 	case $TERM in
 	'screen'*|'tmux'*)
 		print -Pn -- '\e_\005{g}%n\005{-}@\005{m}%m\005{-} \005{B}%~\005{-} | %(!.sudo .) '
 		print -n -- "${(q)1}\e\\"
 		;;
 	*)
-		print -Pn -- "\e]2;${_app_name} | %n@%m > %~ | %(!.sudo .) "
+		print -Pn -- "\e]2;%n@%m > %~ | %(!.sudo .) "
 		print -n -- "${(q)1}\a"
 		;;
 	esac
